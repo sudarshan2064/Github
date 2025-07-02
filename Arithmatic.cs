a6a7cf059151f86b70400091e17b22692aa0cf64
sudarshan2064 <poudelsudarshan141@gmail.com>
2025-07-02 12:05:44 +0545

    Added

---
+++ Arithmatic.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Arithmetic
+{
+   public static int Add(int a, int b) {
+        return a + b;
+   }
+
+    }
+
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+
+class Program
+{
+    public static void Main()
+    {
+        int num1, num2;
+        Console.WriteLine("enter two number");
+        num1 = int.Parse(Console.ReadLine());
+        num2 = int.Parse(Console.ReadLine());
+        int sum = Arithmetic.Add(num1, num2);
+        Console.WriteLine("summ is  "+sum);
+    }
+}
